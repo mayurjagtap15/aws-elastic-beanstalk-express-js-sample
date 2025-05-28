a96457065068b1349491901950678ee043ee9e3f
Mayur Jagtap <40573192+mayurjagtap15@users.noreply.github.com>
2025-05-27 20:31:28 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hi My name is Mayur Jagtap and i am deploying first AWS ElasticbeanStalk Cloud Projecton EC2!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
